ffb0514031c1778f13925748c7009753596e791c
dja <iraneljean21@gmail.com>
2024-08-14 13:13:48 -0400

    Stuff
    8/14 1:13

--- Assets/NailGunScript.cs
+++ Assets/NailGunScript.cs
@@ -7,19 +7,53 @@ public class NailGunScript : MonoBehaviour
     
     public GameObject nailPrefab;
     public Vector3 offset;
-    
+   
+    public GameObject leftNailPrefab;
+    public GameObject rightNailPrefab;
+    public int playerFacing;
+    public int NailCount;
+    public bool isNailGunAvailable;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NailCount = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.A))
+        {
+            playerFacing = 1;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            playerFacing = -1;
+        }
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(nailPrefab, GetComponent<Transform>().position, Quaternion.identity);
+            if(playerFacing == 1)
+            { 
+                Instantiate(rightNailPrefab, GetComponent<Transform>().position, Quaternion.identity);
+                NailCount -= 1;
+            }
+            if (playerFacing == -1)
+
+            {
+                Instantiate(leftNailPrefab, GetComponent<Transform>().position, Quaternion.identity);
+                NailCount -= 1;
+            }
+        }
+
+        if (NailCount == 0)
+        {
+            Destroy(gameObject);
         }
         
     }
